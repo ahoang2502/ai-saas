62cad2a748bc50648648a88ea33bb45caae7a3a7
ahoang2502 <athoang2502@gmail.com>
2023-08-16 16:09:14 +1000

    ...

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -15,6 +15,7 @@ import {
   Settings,
   VideoIcon,
 } from "lucide-react";
+import FreeCounter from "./FreeCounter";
 
 const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
 
@@ -63,12 +64,16 @@ const routes = [
   },
 ];
 
-const Sidebar = () => {
+interface SidebarProps {
+  apiLimitCount: number;
+}
+
+const Sidebar = ({ apiLimitCount = 0 }: SidebarProps) => {
   const pathname = usePathname();
 
   return (
-    <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
-      <div className="px-3 py-2 flex-1">
+    <div className="space-y-4 py-4 flex flex-col h-full justify-between bg-[#111827] text-white ">
+      <div className="px-3 py-2 flex-1 ">
         <Link href="/dashboard" className="flex items-center pl-3 mb-14">
           <div className="relative w-8 h-8 mr-4">
             <Image fill src="/logo.png" alt="logo" />
@@ -83,8 +88,11 @@ const Sidebar = () => {
             <Link
               href={route.href}
               key={route.href}
-              className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg", 
-              pathname === route.href ? "text-white bg-white/10" : "text-zinc-400"
+              className={cn(
+                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg",
+                pathname === route.href
+                  ? "text-white bg-white/10"
+                  : "text-zinc-400"
               )}
             >
               <div className="flex items-center flex-1">
@@ -94,6 +102,10 @@ const Sidebar = () => {
             </Link>
           ))}
         </div>
+      </div>
+
+      <div className="absolute bottom-0 w-full">
+        <FreeCounter apiLimitCount={apiLimitCount} />
       </div>
     </div>
   );
